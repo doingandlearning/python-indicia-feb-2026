6f54e984f58da01bdccd7dc957d4f99aa7056d86
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-02-20 09:49:32 +0000

    adds pytest

--- 12_testing/maths_test.py
+++ 12_testing/maths_test.py
@@ -1,4 +1,5 @@
 from maths import add
+import pytest
 
 def test_adding_two_numbers_gives_correct_answer():
   # Arrange     Given
@@ -11,3 +12,24 @@ def test_adding_two_numbers_gives_correct_answer():
 
   # Assert      Then
   assert result == expected
+
+def test_adding_two_negative_numbers_gives_correct_answer():
+  assert add(-2, -2) == -4
+
+
+# big numbers
+# small numbers
+# negative, positive
+# positive, negative
+# decimals 
+
+# parameterized testing
+# decorator
+@pytest.mark.parametrize("num1, num2, expected", [
+  (1_000_000, 1_000_000, 2_000_000),
+  (1,1,2),
+  (-10, 5, -5),
+  (10, -5, 5)
+])
+def test_adding_various_numbers_gives_correct_answer(num1, num2, expected):
+  assert add(num1, num2) == expected
