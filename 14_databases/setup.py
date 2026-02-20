4b6f396b859e4c839bfe2de86f061ffdb5fa9a97
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-02-20 14:21:27 +0000

    adds add user

--- 14_databases/setup.py
+++ 14_databases/setup.py
@@ -9,5 +9,33 @@ Base = declarative_base()
 # set echo to True if you want to debug the SQL queries
 engine = create_engine('sqlite:///library.db', echo=False)
 
-
 Session = sessionmaker(bind=engine)
+
+class Author(Base):
+  __tablename__ = "authors"  # let's me control what the table is called
+
+  author_id = Column(String, primary_key=True)
+  name = Column(String, nullable=False)
+  email = Column(String, nullable=True, unique=True)
+  country = Column(String)
+  created_at = Column(DateTime, default=datetime.now)
+
+  def __repr__(self):
+    return f"<Author(author_id='{self.author_id}', name='{self.name}')>"
+
+Base.metadata.create_all(engine)
+
+# CRUD -> Create, Read, Update, Delete
+def add_author(author_id, name, email, country=None):
+  session = Session()
+  try:
+    author = Author(author_id=author_id, name=name, email=email, country=country)
+    session.add(author)
+    session.commit()
+    return True
+  except Exception as e:
+    session.rollback()
+    print(f"Error {e}")
+    return False
+  finally:
+    session.close()
